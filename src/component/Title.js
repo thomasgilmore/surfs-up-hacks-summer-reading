e0799d80b97371a8dfa8c933e7428df285c038bd
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-11 19:09:55 -0400

    Title.js & title.css

---
+++ src/component/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './title.css';
+
+export default function Title() {
+    return (
+        <div className="headingDiv">
+            <h1 className="heading">Summer Reading</h1>
+        </div>
+    )
+}
