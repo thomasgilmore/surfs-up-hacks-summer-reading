784591826b72c0f0df6b610bf835cb7df25dfd55
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-11 20:59:18 -0400

    amazon link

--- src/component/Book.js
+++ src/component/Book.js
@@ -5,7 +5,9 @@ class Book extends React.Component {
     render() {
         return (
             <div className="book">
+            <a href={this.props.amazonLink} target="_blank">
                 <img src={this.props.img} alt="Book Cover" className="bookCover" />
+            </a>
                 <p className="bookTitle">{this.props.bookTitle}</p>
                 <p className="bookAuthor">{this.props.bookAuthor}</p>
             </div>
